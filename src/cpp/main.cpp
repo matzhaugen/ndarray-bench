ad692f49fb68634048292647f82900996a1cfdb8
Matz Haugen <matz.haugen@cognite.com>
2020-12-25 22:12:00 +0100

    add dot product

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -6,13 +6,21 @@ using namespace Eigen;
 using namespace std;
 
 int main() {
-	int p = 100;
+	int p = 1000;
 	MatrixXd matrix1 = MatrixXd::Zero(p, p);
 	MatrixXd matrix2 = MatrixXd::Ones(p, p);
+	MatrixXd matrix3(p, p);
 		
 	auto start = chrono::steady_clock::now();
 	matrix1 = matrix2;
 	auto end = chrono::steady_clock::now();
+	cout << "Elapsed time in microseconds : "
+	  << chrono::duration_cast<chrono::microseconds>(end - start).count()
+	  << " us" << endl;
+
+	start = chrono::steady_clock::now();
+	matrix3 = matrix2.cwiseProduct(matrix1);
+	end = chrono::steady_clock::now();
 	cout << "Elapsed time in microseconds : "
 	  << chrono::duration_cast<chrono::microseconds>(end - start).count()
 	  << " us" << endl;
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,39 @@
 #[macro_use(azip)]
 extern crate ndarray;
-use ndarray::Array2;
+use ndarray::Axis;
+use ndarray::{Array2, ArrayView2};
+use rand;
+use rand::distributions::{Distribution, Uniform};
 use std::time::Instant;
+
+pub fn resample<'a>(data: &'a ArrayView2<f64>, n: &usize) -> Array2<f64> {
+    // let resampled = data.sample_axis(Axis(0), *n, SamplingStrategy::WithReplacement); // for v ndarray = 0.14
+    let between = Uniform::from(0..*n);
+    let mut rng = rand::thread_rng();
+    let mut indeces = Vec::with_capacity(*n);
+    for _i in 0..*n {
+        indeces.push(between.sample(&mut rng));
+    }
+
+    let resampled = data.select(Axis(1), &indeces[..]);
+
+    resampled
+}
+
 fn main() {
-    let p: usize = 100;
-    let mat1 = Array2::<f32>::ones((p, p));
+    let p: usize = 1000;
+    let mat1 = Array2::<f64>::ones((p, p));
     let mut mat2 = mat1.clone();
-    let start = Instant::now();
+    let mut start = Instant::now();
     azip!((m1 in &mat1, m2 in &mut mat2) *m2 = -m1);
-    let duration = start.elapsed();
+    let mut duration = start.elapsed();
     println!("Time elapsed is: {:?}", duration);
+    start = Instant::now();
+    let _mat3 = mat1.dot(&mat2);
+    duration = start.elapsed();
+    println!("Time elapsed in dot product is: {:?}", duration);
+    start = Instant::now();
+    let _mat3 = resample(&mat1.view(), &p).dot(&mat2);
+    duration = start.elapsed();
+    println!("Time elapsed in dot product is: {:?}", duration);
 }
